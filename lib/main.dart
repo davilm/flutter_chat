b7b302721364fc255da2d1a5b7217b9070c8de27
Davi Leal <lealm.davi@gmail.com>
2021-05-24 12:15:32 -0300

    feat: Initialize Firebase

---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+// Import the firebase_core plugin
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_chat/views/auth_screen.dart';
+import 'package:flutter_chat/views/chat_screen.dart';
+import 'package:flutter_chat/views/widgets/auth_form.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(App());
+}
+
+/// We are using a StatefulWidget such that we only create the [Future] once,
+/// no matter how many times our widget rebuild.
+/// If we used a [StatelessWidget], in the event where [App] is rebuilt, that
+/// would re-initialize FlutterFire and make our application re-enter loading state,
+/// which is undesired.
+class App extends StatefulWidget {
+  // Create the initialization Future outside of `build`:
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        backgroundColor: Colors.blue,
+        accentColor: Colors.red,
+      ),
+      home: FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Container(
+                child: Text("Algo deu errado!"),
+              ),
+            );
+          }
+
+          if (snapshot.connectionState == ConnectionState.done) {
+            return AuthScreen();
+          }
+          return Center(
+            child: Container(child: CircularProgressIndicator()),
+          );
+        },
+      ),
+    );
+  }
+}
