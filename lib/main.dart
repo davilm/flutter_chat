b09e912cf879413df0ae1d13d48ad328812be4f4
Davi Leal <lealm.davi@gmail.com>
2021-05-29 23:00:45 -0300

    feat: add push notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,48 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-// Import the firebase_core plugin
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
 import 'package:flutter_chat/views/auth_screen.dart';
 import 'package:flutter_chat/views/chat_screen.dart';
 
+/// Create a [AndroidNotificationChannel] for heads up notifications
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+  'high_importance_channel', // id
+  'High Importance Notifications', // title
+  'This channel is used for important notifications.', // description
+  importance: Importance.high,
+);
+
+/// Initialize the [FlutterLocalNotificationsPlugin] package.
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+/// To verify things are working, check out the native platform logs.
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+  print('Handling a background message ${message.messageId}');
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+          AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
 
   runApp(App());
 }
@@ -28,6 +62,26 @@ class _AppState extends State<App> {
   /// The future is part of the state of our widget. We should not call `initializeApp`
   /// directly inside [build].
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  void showNotification() {
+    setState(() {
+      flutterLocalNotificationsPlugin.show(
+        0,
+        "Flutter Chat",
+        "Your new message is here",
+        NotificationDetails(
+            android: AndroidNotificationDetails(
+          channel.id,
+          channel.name,
+          channel.description,
+          importance: Importance.high,
+          color: Colors.blue,
+          playSound: true,
+          icon: '@mipmap/ic_launcher',
+        )),
+      );
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,7 +108,70 @@ class _AppState extends State<App> {
               stream: FirebaseAuth.instance.authStateChanges(),
               builder: (context, userSnapshot) {
                 if (userSnapshot.hasData) {
-                  return ChatScreen();
+                  @override
+                  void initState() {
+                    super.initState();
+                    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+                      RemoteNotification notification = message.notification;
+                      AndroidNotification android =
+                          message.notification?.android;
+                      if (notification != null && android != null) {
+                        flutterLocalNotificationsPlugin.show(
+                            notification.hashCode,
+                            notification.title,
+                            notification.body,
+                            NotificationDetails(
+                              android: AndroidNotificationDetails(
+                                channel.id,
+                                channel.name,
+                                channel.description,
+                                color: Colors.blue,
+                                playSound: true,
+                                icon: '@mipmap/ic_launcher',
+                              ),
+                            ));
+                      }
+                    });
+
+                    FirebaseMessaging.onMessageOpenedApp
+                        .listen((RemoteMessage message) {
+                      print('A new onMessageOpenedApp event was published!');
+                      RemoteNotification notification = message.notification;
+                      AndroidNotification android =
+                          message.notification?.android;
+                      if (notification != null && android != null) {
+                        showDialog(
+                            context: context,
+                            builder: (_) {
+                              return AlertDialog(
+                                title: Text(notification.title),
+                                content: SingleChildScrollView(
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      Text(notification.body),
+                                    ],
+                                  ),
+                                ),
+                              );
+                            });
+                      }
+                    });
+                  }
+
+                  return SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        ChatScreen(),
+                        FloatingActionButton(
+                          onPressed: showNotification,
+                          tooltip: 'increment',
+                          child: Icon(Icons.add),
+                        ),
+                      ],
+                    ),
+                  );
                 } else {
                   return AuthScreen();
                 }
